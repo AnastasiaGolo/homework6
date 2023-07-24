25dc295649396074859a8a832a81f277b56bde13
Anastasia <agolovanyova@yandex.ru>
2023-07-24 19:02:15 +0300

    add "дз"

---
+++ homework6.java
@@ -0,0 +1,99 @@
+package seminar.homework6;
+
+import java.util.*;
+
+class Laptop {
+    private static int count = 0;
+    private int id;
+    private String model;
+    private int ram;
+    private int storage;
+    private String os;
+    private String color;
+
+    public Laptop(String model, int ram, int storage, String os, String color) {
+        this.id = ++count;
+        this.model = model;
+        this.ram = ram;
+        this.storage = storage;
+        this.os = os;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", ram=" + ram +
+                ", storage=" + storage +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
+
+public class homework6 {
+    public static void main(String[] args) {
+        Set<Laptop> laptops = new HashSet<>();
+        laptops.add(new Laptop("Model 1", 8, 256, "Windows 10", "Black"));
+        laptops.add(new Laptop("Model 2", 16, 512, "Windows 10", "Silver"));
+        laptops.add(new Laptop("Model 3", 8, 512, "MacOS", "Grey"));
+        laptops.add(new Laptop("Model 4", 16, 256, "Linux", "White"));
+        laptops.add(new Laptop("Model 5", 8, 512, "Windows 10", "Black"));
+
+        Map<Integer, String> criteria = new HashMap<>();
+        criteria.put(1, "оперативной памяти");
+        criteria.put(2, "объёма памяти");
+        criteria.put(3, "операционной системы");
+        criteria.put(4, "цвета");
+
+        Map<String, Integer> filterParams = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        for (Map.Entry<Integer, String> entry : criteria.entrySet()) {
+            System.out.println("Введите минимальное значение для " + entry.getValue() + ":");
+            int value = scanner.nextInt();
+            filterParams.put(entry.getValue(), value);
+        }
+
+        Set<Laptop> filteredLaptops = new HashSet<>();
+        for (Laptop laptop : laptops) {
+            if (laptop.getRam() >= filterParams.get("RAM") &&
+                    laptop.getStorage() >= filterParams.get("Storage") &&
+                    laptop.getOs().equals(filterParams.get("Operating System")) &&
+                    laptop.getColor().equals(filterParams.get("Color"))) {
+                filteredLaptops.add(laptop);
+            }
+        }
+        
+        System.out.println("\nFiltered Laptops:");
+        for (Laptop laptop : filteredLaptops) {
+            System.out.println(laptop);
+        }
+    }
+}
